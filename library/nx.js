79cc1be181b4d8f0862507b435eaa2bbd82d29f6
afeiship <1290657123@qq.com>
2019-11-23 14:06:59 +0800

    add: lib code

---
+++ library/nx.js
@@ -0,0 +1,20 @@
+(function(root, factory) {
+  if (typeof define === 'function' && define.amd) {
+    define([], factory);
+  } else if (typeof exports === 'object') {
+    module.exports = factory();
+  } else {
+    root.nx = factory();
+  }
+})(this, function() {
+  var root =
+    (typeof self == 'object' && self.self === self && self) ||
+    (typeof global == 'object' && global.global === global && global) ||
+    this ||
+    {};
+
+  var nx = global.nx || {};
+  nx.GLOBAL = root;
+
+  return nx;
+});
